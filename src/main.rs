54d6accdaed0dc9fe53709cb17d8b1ea7719e435
Tatu Pesonen <tatu@narigon.dev>
2022-11-03 21:14:22 +0200

    fix: make clippy happy

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
 extern crate dotenv;
-use std::{collections::HashMap, time::Duration};
 use dotenv::dotenv;
-use reqwest::{StatusCode, };
 use std::env;
+use std::{collections::HashMap, time::Duration};
+#[allow(clippy::or_fun_call)]
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-		dotenv().ok();
-		let key = env::var("KEY").expect("No KEY provided.");
-		let vault_addr = env::var("VAULT_ADDR").expect("No VAULT_ADDR provided.");
-		let interval: u64 = env::var("INTERVAL").unwrap_or("5".into()).parse().expect("Non-numeric value passed for INTERVAL.");
+    dotenv().ok();
+    let key = env::var("KEY").expect("No KEY provided.");
+    let vault_addr = env::var("VAULT_ADDR").expect("No VAULT_ADDR provided.");
+    let interval: u64 = env::var("INTERVAL")
+        .unwrap_or("5".to_string())
+        .parse()
+        .expect("Non-numeric value passed for INTERVAL.");
 
-		let client = reqwest::blocking::Client::new();
-		let unseal_url = format!("{}/v1/sys/unseal", vault_addr);
-		let health_url = format!("{}/v1/sys/health", vault_addr);
-		let mut req = HashMap::new();
-		req.insert("key", key);
+    let client = reqwest::blocking::Client::new();
+    let unseal_url = format!("{}/v1/sys/unseal", vault_addr);
+    let health_url = format!("{}/v1/sys/health", vault_addr);
+    let mut req = HashMap::new();
+    req.insert("key", key);
 
-		loop {
-			// Check if the vault is sealed.
-			let res = client.get(&health_url).send();
-			match res {
-				Ok(data) => {
-					match data.status() {
-						// 503
-						StatusCode::SERVICE_UNAVAILABLE => {
-							// Send 
-							println!("Vault is sealed, trying to unseal...");
-							let res = client.post(&unseal_url).json(&req).send();
-							if let Ok(_) = res {
-								println!("Vault unsealed.");
-							}
-						}
-						_ => ()
-					}
-				}
-				Err(e) => {
-					println!("Unable to query Vault health: {}", e);
-				}
-			}
-			std::thread::sleep(Duration::from_secs(interval))
-		}
+    loop {
+        // Check if the vault is sealed.
+        let res = client.get(&health_url).send();
+        match res {
+            Ok(_) => {
+                println!("Vault is sealed, trying to unseal...");
+                let res = client.post(&unseal_url).json(&req).send();
+								match res {
+									Ok(_) => println!("Vault unsealed."),
+									Err(err) => println!("Unable to unseal Vault: {}", err)
+								}
+            }
+            Err(e) => {
+                println!("Unable to query Vault health: {}", e);
+            }
+        }
+        std::thread::sleep(Duration::from_secs(interval))
+    }
 }
