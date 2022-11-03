a3636bd0c524f99e96e9f0e659cb35197812c102
Tatu Pesonen <tatu@narigon.dev>
2022-11-04 00:25:41 +0200

    feat: allow multiple vaults and multiple keys to be provided

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ docker run -e KEY=1234 -e VAULT_ADDR=http://localhost:8200 nnari/unseald
 
 ## Config options
 unseald looks at the following environment variables:
-- KEY - The key to unseal with
-- VAULT_ADDR - Vault address
+- KEY - The key to unseal with. If you want to provide multiple unseal keys, you can separate them with commas.
+- VAULT_ADDR - Vault address. Also to target multiple Vaults, you can separate with commas.
 - INTERVAL - Defaults to 5, how often to check `/v1/sys/health`
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,51 @@
 extern crate dotenv;
 use dotenv::dotenv;
+use reqwest::StatusCode;
 use std::env;
 use std::{collections::HashMap, time::Duration};
 #[allow(clippy::or_fun_call)]
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let key = env::var("KEY").expect("No KEY provided.");
-    let vault_addr = env::var("VAULT_ADDR").expect("No VAULT_ADDR provided.");
+    let keys = env::var("KEY").expect("No KEY provided.");
+    let vault_addrs = env::var("VAULT_ADDR").expect("No VAULT_ADDR provided.");
     let interval: u64 = env::var("INTERVAL")
         .unwrap_or("5".to_string())
         .parse()
         .expect("Non-numeric value passed for INTERVAL.");
 
     let client = reqwest::blocking::Client::new();
-    let unseal_url = format!("{}/v1/sys/unseal", vault_addr);
-    let health_url = format!("{}/v1/sys/health", vault_addr);
-    let mut req = HashMap::new();
-    req.insert("key", key);
 
     loop {
-        // Check if the vault is sealed.
-        let res = client.get(&health_url).send();
-        match res {
-            Ok(_) => {
-                println!("Vault is sealed, trying to unseal...");
-                let res = client.post(&unseal_url).json(&req).send();
+        for addr in vault_addrs.split(',').into_iter() {
+            let unseal_url = format!("{}/v1/sys/unseal", addr);
+            let health_url = format!("{}/v1/sys/health", addr);
+            for (mut idx, key) in keys.split(",").into_iter().enumerate() {
+								idx += 1;
+                let mut req = HashMap::new();
+                req.insert("key", key);
+                let res = client.get(&health_url).send();
                 match res {
-                    Ok(_) => println!("Vault unsealed."),
-                    Err(err) => println!("Unable to unseal Vault: {}", err),
+                    Ok(data) => {
+                        if data.status() != StatusCode::OK {
+                            println!("Vault at {} is sealed, trying to unseal with key portion {}...", addr, idx);
+                            let res = client.post(&unseal_url).json(&req).send();
+                            match res {
+                                Ok(data) => {
+																	if data.status() == StatusCode::OK {
+																		println!("Vault at {} provided with key portion {}.", addr, idx)
+																	} else {
+																		println!("Unable to unseal Vault: {}", data.status());
+																	}
+																},
+                                Err(err) => println!("Unable to unseal Vault: {}", err),
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("Unable to query Vault health: {}", e);
+                    }
                 }
-            }
-            Err(e) => {
-                println!("Unable to query Vault health: {}", e);
             }
         }
         std::thread::sleep(Duration::from_secs(interval))
